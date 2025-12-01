5ff24d96802bd3e3a282788bfa27d6a21ceb0e0a
Malo Kerebel <malo@kerebel.xyz>
2025-12-02 00:15:14 +0100

    Toutes les figures de la section 1.6 finies

--- figures/chapter1/figure_compile.sh
+++ figures/chapter1/figure_compile.sh
@@ -1,4 +1,5 @@
 pdflatex triple_product.tex
+python cross_section_data/plot.py
 pdflatex potential_energy.tex
 pdflatex cross_section.tex
 pdflatex sigma_v.tex
@@ -6,6 +7,20 @@ pdflatex integrand.tex
 pdflatex sigma_v_3_rections.tex
 pdflatex n_tau_E_ignition.tex
 pdflatex power_comp.tex
-lualatex contour.tex
-lualatex wireframe.tex
-lualatex stability_condition
+yes "" | lualatex --shell-escape Contour.tex
+yes "" | lualatex wireframe.tex
+yes "" | lualatex stability_condition.tex
+pdflatex schematic_tokamak_1.tex
+pdflatex schematic_tokamak_2.tex
+pdflatex schematic_tokamak_3.tex
+pdflatex schematic_tokamak_4.tex
+pdflatex schematic_tokamak_5.tex
+pdflatex schematic_tokamak_6.tex
+pdflatex schematic_tokamak_7.tex
+pdflatex schematic_tokamak_8.tex
+pdflatex schematic_tokamak_9.tex
+
+rm *.aux
+rm *.log
+rm *.script
+rm *.dat
