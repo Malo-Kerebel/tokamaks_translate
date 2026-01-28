cf8b51b66f717578413a5bf0813f40e2d21b8ee0
Malo Kerebel <malo@kerebel.xyz>
2026-01-28 21:13:50 +0100

    section 2.6 terminé et début de la section 2.7, correction des mathbf en bme meilleure typographie

--- figures/chapter2/figure_compile.sh
+++ figures/chapter2/figure_compile.sh
@@ -1,3 +1,10 @@
 pdflatex charge_sheet.tex
 cd shielding/ && gfortran potential.f90 && ./a.out && cd ..
 pdflatex potential.tex
+pdflatex displaced_electrons.tex
+pdflatex displaced_electrons_2.tex
+pdflatex grad_B.tex
+pdflatex ExB.tex
+pdflatex drift_grad_B.tex
+pdflatex ion_drift.tex
+pdflatex polarisation_drift.tex
